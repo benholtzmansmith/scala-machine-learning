bd939a8bd666ba6e49e9b52f825ce508093d48e9
benholtzmansmith <benjamin.holtzman.smith@gmail.com>
2016-05-09 00:30:48 -0400

    formatting, optimizing imports

--- src/main/scala/Algos.scala
+++ src/main/scala/Algos.scala
@@ -1,7 +1,6 @@
 package machine.learning.algos
 
-import scala.math.pow
-import MathHelp._
+import machine.learning.algos.MathHelp._
 
 object Algos {
   /**
@@ -9,23 +8,25 @@ object Algos {
     *
     * Provide a list of touples of predicted values and true labels
     *
-    * */
+    **/
 
   type PredictedValue = Double
   type TrueValue = Double
   type Accuracy = Double
 
-  def costFunciton(input:List[(PredictedValue, TrueValue)]):Accuracy = {
+  def costFunciton(input: List[(PredictedValue, TrueValue)]): Accuracy = {
     val multipler = 0.5 // to keep the math simple when taking the derivative
     val exponent = 2 //to keep the answer positive
 
-    multipler * input.foldLeft(0.0){ case (acc, (pv, tv)) => acc + (pv - tv)}.**(exponent)
+    multipler * input.foldLeft(0.0) { case (acc, (pv, tv)) => acc + (pv - tv) }.**(exponent)
   }
 }
 
 
-object MathHelp{
-  implicit class Pow(base:Double){
-    def **(toThe:Double) = math.pow(base, toThe)
+object MathHelp {
+
+  implicit class Pow(base: Double) {
+    def **(toThe: Double) = math.pow(base, toThe)
   }
+
 }
